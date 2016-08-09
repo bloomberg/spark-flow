97688fd2b3ad1d947acff49464a863f03c07fd60
Anika Mohindra <anika.mohindra@gmail.com>
2016-08-09 13:44:54 -0700

    Graph classes update
    Collapsed node/edge/graph classes into util.

---
+++ src/main/scala/com/bloomberg/sparkflow/graphs/Util.scala
@@ -0,0 +1,64 @@
+package com.bloomberg.sparkflow.graphs
+
+import com.bloomberg.sparkflow._
+import com.bloomberg.sparkflow.dc.{DC, Dependency}
+import scala.collection.mutable._
+
+/**
+  * Created by amohindra2 on 8/4/16.
+  */
+
+
+object Util {
+
+  case class Edge(src: String, dest: String)
+  case class Node(id: String)
+  case class Graph(nodes: scala.collection.mutable.HashMap[String, Node], edges: List[Edge])
+
+  //Find top-level nodes.
+  def getGrandparents(x: Dependency[_]) : List[Dependency[_]] ={
+    var temp = new ListBuffer[Dependency[_]]
+
+    def grandFinder(y: Dependency[_]){
+      if(y.parents.nonEmpty){
+        for(a <- y.parents){
+          grandFinder(a)
+        }
+      } else {
+        temp += y
+      }
+    }
+    grandFinder(x)
+
+    temp.toList.distinct
+  }
+
+
+  //Build DAG from given starter node. Depends on getGrandparents.
+  def buildDAG(starter: Dependency[_]) : Graph ={
+    val grands = getGrandparents(starter)
+
+    val nodeMap = scala.collection.mutable.HashMap.empty[String, Node]
+    var edgeList = new ListBuffer[Edge]
+
+    def buildDAG2(x: Dependency[_]){
+      nodeMap.update(x.getSignature , new Node(x.getSignature))
+
+      if(x.children.nonEmpty){
+        for(a <- x.children){
+          edgeList += new Edge(x.getSignature, a.getSignature)
+          buildDAG2(a)
+        }
+      }
+
+    }
+
+    for (b <- grands){
+      buildDAG2(b)
+    }
+
+    new Graph(nodeMap, edgeList.toList)
+
+  }
+
+}
